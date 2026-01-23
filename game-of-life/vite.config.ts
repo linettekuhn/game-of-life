90f6a8f0f62b03c2835a5db397808e359ef3daff
Linette Kühn <139412656+linettekuhn@users.noreply.github.com>
2026-01-23 01:51:15 -0500

    Prepare for deployment with github pages

--- game-of-life/vite.config.ts
+++ game-of-life/vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/game-of-life/",
   resolve: {
     alias: {
       GameOfLifeModule: path.resolve(__dirname, "./GameOfLifeModule.js"),
